4a6ab321319389730824a21491334839ff0da43a
Dilip-kumar-reddy <91590018+Dilip-kumar-reddy@users.noreply.github.com>
2022-04-29 03:07:05 +0530

    some changes in route.php,schedule.php ,tracks.php,trains.php.

--- tracks.php
+++ tracks.php
@@ -8,6 +8,27 @@
   <body>
     <?php include("sidebar.php"); ?>
     <div class="main">
+    <!-- add insert etc stuff here -->
+    <table>
+        <tr>
+          <th>Track Id</th>
+          <th>Station Id 1</th>
+          <th>Station Id 2</th>
+          
+        </tr>
+        <?php
+          include("connect.php");
+          $stmt = $conn->query('SELECT * FROM track');
+          if ($stmt->num_rows > 0) {
+            while ($row = $stmt->fetch_assoc()) {
+              echo "<tr><td>" . $row["track_id"] . "</td><td>" . $row["station_id_1"] . "</td>
+              <td>" . $row["station_id_2"] .  "</td>
+              </tr>
+              ";
+            }
+          }
+        ?>
+      </table>
     
     </div>
   </body>
