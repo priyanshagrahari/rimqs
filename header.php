80403f25a8af3ea4473a3cfde5a1abb225947802
priyansh agrahari <75441026+priyansh-agrahari@users.noreply.github.com>
2022-05-03 23:11:15 +0530

    changed base.php to index.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
   echo '
     <h1 class="heading">
-    <a href="base.php">Railway Query System</a>
+    <a href="index.php">Railway Query System</a>
     </h1>
     <title>RQS</title>
     <link rel="stylesheet" href="css/style.css">
--- index.php
+++ index.php
@@ -16,10 +16,15 @@
         The aim of RQS is to provide its users with an easy to use interface to
 query information regarding stations, trains, tracks, the schedules of trains and the
 routes of trains.
-      </p> <br> <br>
-      <p>
-        To begin, select one of the fields from the sidebar.
       </p>
+      <p>
+        To add/modify/remove table entries, select one of the tables from the sidebar.
+      </p>
+      <p>
+        To create a new query, click on the button below:
+      </p>
+      <input type="checkbox" id="query" style="display:none;">
+      <label for="query" class="popup_button">QUERY</label>
     </div>
   </body>
 
